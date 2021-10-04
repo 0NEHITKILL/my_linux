5179c07287d2cefd9d092077e29aae03e792cd27
Gustavo <40724636+0NEHITKILL@users.noreply.github.com>
2021-10-04 15:49:47 -0300

    Update

--- linux.sh
+++ linux.sh
@@ -1,27 +1,27 @@
-sudo apt install gnome-tweaks
+printf "Update"
+sudo apt update  && sudo apt upgrade 
+printf "Gnome Tweaks" 
+sudo apt install gnome-tweaks 
 printf "Snap"
 sleep 2
-sudo apt install snapd
+sudo apt install snapd 
 printf "Authy"
 sleep 2
-sudo snap install authy
+sudo snap install authy 
 printf "Notion"
 sleep 2
-wget https://notion.davidbailey.codes/notion-linux.list
-sudo mv notion-linux.list /etc/apt/sources.list.d/notion-linux.list
+wget https://notion.davidbailey.codes/notion-linux.list &&
+sudo mv notion-linux.list /etc/apt/sources.list.d/notion-linux.list &&
 sudo apt update && sudo apt install notion-desktop
 printf "VSCode"
 sleep 2
-sudo snap install code --classic
+sudo snap install code --classic 
 printf "Git"
 sudo apt install git
-printf "Open Drive"
-sleep 2
-sudo snap install odrive-unofficial
 printf "Shell Extension"
 sudo apt install gnome-shell-extensions
 printf "Theme"
-git clone https://github.com/Jannomag/Yaru-Colors.git
+git clone https://github.com/Jannomag/Yaru-Colors.git &&
 cd Yaru-Colors/ && ./install.sh
 printf "Finalizando"
 sudo apt-get autoclean
